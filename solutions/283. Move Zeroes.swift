37de5588879ab1eff448f38cf54e2bd3b4e863ba
Nikita Kononenko <vmeansdev@gmail.com>
2021-03-16 12:13:46 +0300

    40 ms

--- solutions/283. Move Zeroes.swift
+++ solutions/283. Move Zeroes.swift
@@ -1,10 +1,15 @@
 class Solution {
     func moveZeroes(_ nums: inout [Int]) {
-        for (index, num) in nums.enumerated().reversed() {
-            if num == 0 {
-                nums.remove(at: index)
-                nums.append(num)
+        var i = 0
+        for num in nums {
+            if num != 0 {
+                nums[i] = num
+                i += 1
             }
+        }
+        while i < nums.count {
+            nums[i] = 0
+            i += 1
         }
     }
 }
