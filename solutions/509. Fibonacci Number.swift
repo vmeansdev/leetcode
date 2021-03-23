02a861d0225cefa3387510f65600c4d05cf37162
Nikita Kononenko <vmeansdev@gmail.com>
2021-03-23 11:10:17 +0300

    0 ms

--- solutions/509. Fibonacci Number.swift
+++ solutions/509. Fibonacci Number.swift
@@ -1,7 +1,13 @@
 class Solution {
+    var calculated: [Int: Int] = [:]
     func fib(_ n: Int) -> Int {
         if n == 0 { return 0 }
-        if n == 1 { return 1}
-        return fib(n - 1) + fib(n - 2)
+        if n == 1 { return 1 }
+        if let result = calculated[n] {
+            return result
+        } else {
+            calculated[n] = fib(n - 1) + fib(n - 2)
+        }
+        return calculated[n, default: 0]
     }
 }
