4b7f37fd4bf00104b7740ca5a8586f4100e6c52a
Nikita Kononenko <vmeansdev@gmail.com>
2021-04-07 19:46:58 +0300

    4 ms

---
+++ solutions/199. Binary Tree Right Side View.swift
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        guard let root = root else {
+            return []
+        }
+        var result: [Int] = []
+        var queue: [TreeNode] = [root]
+        while !queue.isEmpty {
+            var vals: [Int] = []
+            for _ in 0 ..< queue.count {
+                let node = queue.removeFirst()
+                vals.append(node.val)
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            result.append(vals.count > 1 ? vals[vals.count - 1] : vals[0])
+        }
+        return result
+    }
+}
